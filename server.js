ee8bd99a7edd817a2adb51095968850ffbaa5f80
Maksymilian Sobolewski <maksobo80@edu.gdansk.pl>
2025-02-12 12:47:06 +0100

    Leave and delete room, default case

--- server.js
+++ server.js
@@ -20,6 +20,34 @@ wss.on("connection", (socket) => {
             }
             rooms[data.room].push(socket);
             socket.send(JSON.stringify({ type: "room-joined", room: data.room }));
+        } else if (data.type === "leave-room") {
+            if (!rooms[data.roomName]) {
+                socket.send(JSON.stringify({ type: "error", message: "Room does not exist" }));
+                return;
+            }
+            rooms[data.room].members = rooms[data.room].members.filter((member) => member !== socket);
+            socket.send(JSON.stringify({ type: "room-left", room: data.room }));
+
+            if(rooms[data.room].members.length <= 0) {
+                delete rooms[data.room];
+            }
+        } else if (data.type === "delete-room") {
+            if (!rooms[data.roomName]) {
+                socket.send(JSON.stringify({ type: "error", message: "Room does not exist" }));
+                return;
+            }
+
+            if (rooms[data.room].members[0] !== socket) {
+                socket.send(JSON.stringify({ type: "error", message: "Only the room creator can delete the room" }));
+                return;
+            }
+
+            rooms[data.room].members.forEach((member) => {
+                member.send(JSON.stringify({ type: "room-deleted", room: data.room }));
+            });
+            delete rooms[data.room];
+        } else {
+            socket.send(JSON.stringify({ type: "error", message: "Invalid message type" }));
         }
     });
 
