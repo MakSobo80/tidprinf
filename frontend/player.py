80ab63239aae7a2e27ac9029f63f38ef6fe97761
AriunaW <ariwejn68@edu.gdansk.pl>
2025-05-28 23:44:54 +0200

    added the set cards method to the  player class

--- frontend/player.py
+++ frontend/player.py
@@ -1,6 +1,9 @@
 class Player:
-def __init__(self, cards, balance, hasFolded, name):
-    self.cards = cards
-    self.balance = balance
-    self.hasFolded = hasFolded
-    self.name = name
+    def __init__(self, cards, balance, hasFolded, name):
+        self.cards = cards
+        self.balance = balance
+        self.hasFolded = hasFolded
+        self.name = name
+
+    def set_cards(self, cards):
+        self.cards = cards
